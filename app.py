6c1fb87a693d15d00cc39907203a8683528f2646
mnguye95 <mnguye95@gmail.com>
2022-07-19 00:15:23 -0700

    added CD Pipeline v3

--- app.py
+++ app.py
@@ -83,4 +83,4 @@ def upload_file():
                         attachment_filename='{}{}'.format('geotagged',ext))
 
 if __name__ == "__main__":
-    app.run(host='0.0.0.0',port=5000,debug=False,threaded=True)
+    app.run(host='127.0.0.1',port=5000,debug=False,threaded=True)
